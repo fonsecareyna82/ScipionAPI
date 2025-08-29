73d13ec4ce3320ceb449512993a1720d3e3af0ac
Yunior C. Fonseca Reyna <cfonsecareyna82@gmail.com>
2025-08-29 07:47:23 +0200

    SignUp

--- app/backend/api/dependencies.py
+++ app/backend/api/dependencies.py
@@ -1,8 +1,10 @@
 # dependencies.py
 
+import os
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from jose import JWTError, jwt
 from app.backend.database import getDb
 from app.backend.models.user import User
 from app.backend.utils.jwt import decodeAccessToken
@@ -11,15 +13,20 @@ from app.backend.utils.jwt import decodeAccessToken
 oauth2Scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+
 def getCurrentUser(token: str = Depends(oauth2Scheme), db: Session = Depends(getDb)) -> User:
-    # Decode JWT token
-    payload = decodeAccessToken(token)
-    if not payload or "sub" not in payload:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        userId: str = payload.get("sub")
+        if userId is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-    # Retrieve user from database
-    user = db.query(User).filter(User.id == int(payload["sub"])).first()
-    if not user:
+    user = db.query(User).filter(User.id == int(userId)).first()
+    if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-
     return user
