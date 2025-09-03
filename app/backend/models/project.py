15256bd663e46cb41af2ed813d6dbd94076296ea
Yunior C. Fonseca Reyna <cfonsecareyna82@gmail.com>
2025-09-04 01:25:31 +0200

    First approach with postgres(Handling projects)

--- app/backend/models/project.py
+++ app/backend/models/project.py
@@ -1,4 +1,10 @@
 # models/project.py
+from pydantic import BaseModel
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app.backend.database import Base
 
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
@@ -8,26 +14,36 @@ from app.backend.database import Base
 class Project(Base):
     __tablename__ = "projects"
 
-    # Primary key
     id = Column(Integer, primary_key=True, index=True)
-
-    # Project name
     name = Column(String, nullable=False)
-
-    # Optional description
     description = Column(String, nullable=True)
-
-    # Status of the project
     status = Column(String, default="active")
-
-    # Foreign key to the user who owns the project
     ownerId = Column(Integer, ForeignKey("users.id"))
-
-    # Timestamp when the project was created
     createdAt = Column(DateTime(timezone=True), server_default=func.now())
-
-    # Timestamp when the project was last updated
     updatedAt = Column(DateTime(timezone=True), onupdate=func.now())
 
-    # Relationship to the User model
     owner = relationship("User", back_populates="projects")
+
+
+class ProjectCreateRequest(BaseModel):
+    name: str
+    description: str
+    created_at: datetime = datetime.now()
+    status: str = 'PENDING'
+    protocolsCount: str = '0'
+    diskUsage: str = '1.4 GB'
+
+
+class ProjectResponse(BaseModel):
+    id: str
+    name: str
+    description: str
+    created_at: datetime
+    status: str
+    protocolsCount: str = '0'
+    diskUsage: str
+
+
+class ProjectUpdateRequest(BaseModel):
+    name: str
+    description: str
