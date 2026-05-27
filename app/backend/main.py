e8b1e1251e7e550fd19b752ab4713685252734af
Yunior C. Fonseca Reyna <34970661+fonsecareyna82@users.noreply.github.com>
2026-05-27 20:59:53 +0200

    Register system router

--- app/backend/main.py
+++ app/backend/main.py
@@ -45,6 +45,7 @@ from app.backend.api.routers.auth_router import router as auth
 from app.backend.api.routers.user_router import router as users
 from app.backend.api.routers.settings_router import router as settingsRouter
 from app.backend.api.routers.coords2d_router import router as coords2dRouter
+from app.backend.api.routers.system_router import router as systemRouter
 from app.backend.utils.error_handlers import registerAllErrorHandlers
 from starlette.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHttpException
@@ -135,6 +136,7 @@ def _buildApiApp() -> FastAPI:
     apiApp.include_router(users)
     apiApp.include_router(settingsRouter)
     apiApp.include_router(coords2dRouter)
+    apiApp.include_router(systemRouter)
 
     @apiApp.get("/health")
     def health_check():
