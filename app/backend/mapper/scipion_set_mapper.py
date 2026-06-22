595a754a40797f662f3dbb980b841fd9316e4705
Yunior C. Fonseca Reyna <cfonsecareyna82@gmail.com>
2026-06-22 15:20:40 +0200

    fix integrated tomogram and ctftomo relations

--- app/backend/mapper/scipion_set_mapper.py
+++ app/backend/mapper/scipion_set_mapper.py
@@ -41,7 +41,7 @@ except Exception:
 
 
 SELF_LABEL = "self"
-NESTED_LOGICAL_TABLES_VERSION = 9
+NESTED_LOGICAL_TABLES_VERSION = 10
 
 
 class ScipionSetPostgresqlMapper(ScipionObjectPostgresqlMapper):
